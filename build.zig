22a269909acd9feb10fde1c74a3d1477e8f851eb
dean <deanvelopermn@gmail.com>
2024-01-11 23:13:25 -0800

    add day option to build test

--- build.zig
+++ build.zig
@@ -4,21 +4,15 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     // make 'dean.zig' available as just "dean"
     const deanModule = b.addModule("dean", .{ .source_file = .{ .path = "src/dean.zig" } });
 
     const test_step = b.step("test", "Runs tests");
+
+    const dayOption = b.option(u32, "day", "Filter tests by day");
 
     var src_dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
     defer src_dir.close();
@@ -27,6 +21,21 @@ pub fn build(b: *std.Build) !void {
     while (try files.next()) |file| {
         if (file.kind != .directory or file.name.len != 5) {
             continue;
+        }
+
+        if (dayOption) |day| {
+            if (day >= 100) {
+                return error.DayLimitIs100;
+            }
+            var dayStr: [2]u8 = undefined;
+            var fbs = std.io.fixedBufferStream(&dayStr);
+            try std.fmt.formatInt(day, 10, .lower, .{ .fill = '0', .alignment = .right, .width = 2 }, fbs.writer());
+            var dayAsDir: [5]u8 = undefined;
+            @memcpy(dayAsDir[0..3], "day");
+            @memcpy(dayAsDir[3..5], &dayStr);
+            if (!std.mem.eql(u8, &dayAsDir, file.name)) {
+                continue;
+            }
         }
 
         if (std.mem.startsWith(u8, file.name, "day0") and file.name.len > 4) {
